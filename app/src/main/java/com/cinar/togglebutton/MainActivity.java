d1a0e486bbfaa88b5d6c3e97ee35ecacdd8e0416
Selim Çınar <btk02672@gmail.com>
2023-08-08 00:45:19 +0300

    Initial commit

---
+++ app/src/main/java/com/cinar/togglebutton/MainActivity.java
@@ -0,0 +1,44 @@
+package com.cinar.togglebutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ToggleButton yesNo;
+    ImageView imageView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        yesNo  = findViewById(R.id.toggleButton);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        textView.setText("Fenerbahçe");
+
+       yesNo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (yesNo.isChecked()){
+                    imageView.setImageResource(R.drawable.fb);
+                    textView.setText("Fenerbahçe");
+                }
+                else {
+                    imageView.setImageResource(R.drawable.tr);
+                    textView.setText("Türkiye");
+                }
+            }
+        });
+
+    }
+}
